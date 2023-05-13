22fb48bf8bcb678fd67f1642d1f6418e99cce6a4
Liran Uziyahu <liranuzistud@gmail.com>
2023-05-13 13:36:52 +0300

    Add authentication , Auto0 - Login/Logout, plus send server the email`s user

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -3,7 +3,7 @@ import Router from './Route/Router';
 function App() {
   return (
     <>
-    <Router></Router>
+      <Router></Router>
     </>
   );
 }
--- app/client/src/pages/chatBot/index.js
+++ app/client/src/pages/chatBot/index.js
@@ -3,9 +3,9 @@ import FormChatBot from './FormChatBot'
 import StepperForm from '../Stepper/StepperForm'
 import { makeStyles } from '@material-ui/styles';
 import ResponeChat from './ResponeChat';
-import Map from '../Maps/Map'
-import Image from '../Unsplash/Image'
-import LoginButton from '../Auto0/Auto0';
+import LoginButton from '../Auto0/LoginButton';
+import LogoutButton from '../Auto0/LogoutButton';
+import { useAuth0 } from "@auth0/auth0-react";
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@1,500&display=swap');
@@ -15,16 +15,22 @@ const Index = () => {
   const [controller , setController]  = useState(0)
   const [responseBot, setResponseBot] = useState('');
   const classes = useStyles();
+  const { user, isAuthenticated ,logout  } = useAuth0();
+
 
   return (
     <div className={classes.warp}>
       {
-        responseBot == '' && controller < 2 ? <StepperForm  props = {{controller:controller,setController:setController }}></StepperForm>:null
-      } 
-        <FormChatBot props = {{setController:setController , controller:controller , setResponseBot:setResponseBot ,responseBot:responseBot}}></FormChatBot>
-        <ResponeChat props={{responseBot:responseBot}}></ResponeChat>
-        {/* <Map></Map> */}
-        {/* <LoginButton></LoginButton> */}
+        !isAuthenticated ? <LoginButton /> :
+          (
+            <>
+              <LogoutButton/>
+              {responseBot === '' && controller < 2 && <StepperForm props={{ controller: controller, setController: setController }} />}
+              <FormChatBot props={{ setController: setController, controller: controller, setResponseBot: setResponseBot, responseBot: responseBot }} />
+              <ResponeChat props={{ responseBot: responseBot }} />
+            </>
+          )
+      }
     </div>
   )
 }
@@ -44,3 +50,4 @@ const useStyles = makeStyles({
     overflowY: 'scroll !important',
   }
 })
+
